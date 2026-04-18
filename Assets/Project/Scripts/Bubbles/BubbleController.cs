cfd68aa0e98cae1792d2277f7a16c4d79e26d53f
IzmaylovNikita <nikitaizmaylov1402@gmail.com>
2026-04-18 12:50:58 +0300

    Fix prefabs

--- Assets/Project/Scripts/Bubbles/BubbleController.cs
+++ Assets/Project/Scripts/Bubbles/BubbleController.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 
 namespace Bubbles
 {
-    public class BubbleController : MonoBehaviour
+    public class BubbleController : MonoBehaviour, IBubbleController
     {
         [SerializeField] private EBubbleType _bubbleType;
+        [SerializeField] private Rigidbody2D _rb;
         [SerializeField] private Animator _animator;
         public EBubbleType BubbleType => _bubbleType;
 
@@ -13,6 +14,19 @@ namespace Bubbles
         public void Burst()
         {
             _animator.SetBool("IsBurst", true);
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (!collision.collider.CompareTag("Wall"))
+                return;
+            Vector2 velocity = _rb.linearVelocity;
+            float speed = velocity.magnitude;
+
+            Vector2 normal = collision.GetContact(0).normal;
+            Vector2 reflected = Vector2.Reflect(velocity.normalized, normal);
+
+            _rb.linearVelocity = reflected * speed; // сохраняем ту же скорость
         }
     }
 }
---
+++ Assets/Project/Scripts/Bubbles/IBubbleController.cs
@@ -0,0 +1,8 @@
+namespace Bubbles
+
+{
+    public interface IBubbleController
+    {
+        void Burst();
+    }
+}
